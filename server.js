7a0da0e96e4611d2eed3c2cb8e2f5bf14fc2d25c
j0shmartin <josh@joshmartin95.com>
2022-03-03 17:41:20 -0500

    CRUD backend complete

---
+++ server.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { PORT = 4000, MONGODB_URL } = process.env;
+const express = require("express");
+const app = express();
+const cors =require("cors");
+const morgan = require("morgan")
+
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+mongoose.connection
+    .on("open", ()=>console.log("connected to mongoose"))
+    .on("close",()=>console.log("disconnected from mongoose"))
+    .on("error",(error)=>console.log(error));
+
+
+const PeopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String,
+});
+
+const People = mongoose.model("People",PeopleSchema)
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+});
+
+app.get("/people",async (req,res)=>{
+    try {
+        res.json(await People.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+app.post("/people", async (req,res)=>{
+    try {
+        res.json(await People.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+app.put("/people/:id", async (req,res)=>{
+    try {
+        res.json(
+            await People.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        );
+    } catch (error) {
+        res.status(404).json(error);
+    }
+});
+
+app.delete("/people/:id", async (req,res)=>{
+    try {
+        res.json(await People.findByIdAndRemove(req.params.id));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+    
+app.listen(PORT, ()=> console.log(`listening on PORT ${PORT}`));
